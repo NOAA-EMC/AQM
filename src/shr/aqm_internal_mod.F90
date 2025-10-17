ad7c22f6e03a5f7933df1245d677d25b8b10e946
KaiWang-NOAA <kai.wang@noaa.gov>
2025-10-17 16:52:13 +0000

    Revert "bring in most of production/AQM.v7 commit 1a87ede (without AERO_EMIS.F)"
    This reverts commit 05adc4dd5ee3edf22a93d55c3e84136c15f812db.

--- src/shr/aqm_internal_mod.F90
+++ src/shr/aqm_internal_mod.F90
@@ -8,12 +8,6 @@ module aqm_internal_mod
     real(ESMF_KIND_R4), dimension(:), pointer :: values => null()
   end type aqm_internal_rate_type
 
-  type aqm_internal_psrc_type
-    integer :: nloc
-    integer :: i
-    integer :: j
-  end type aqm_internal_psrc_type
-
   type aqm_internal_emis_type
     character(len=ESMF_MAXSTR)     :: name
     character(len=ESMF_MAXSTR)     :: type
@@ -48,6 +42,9 @@ module aqm_internal_mod
     character(len=ESMF_MAXSTR), dimension(:),   pointer :: species   => null()
     character(len=ESMF_MAXSTR), dimension(:),   pointer :: units     => null()
     integer,                    dimension(:),   pointer :: dens_flag => null()
+    integer,                    dimension(:),   pointer :: ip        => null()
+    integer,                    dimension(:),   pointer :: jp        => null()
+    integer,                    dimension(:),   pointer :: ijmap     => null()
     real(ESMF_KIND_R4),         dimension(:),   pointer :: lat       => null()
     real(ESMF_KIND_R4),         dimension(:),   pointer :: lon       => null()
     real(ESMF_KIND_R4),         dimension(:),   pointer :: stkdm     => null()
@@ -56,7 +53,6 @@ module aqm_internal_mod
     real(ESMF_KIND_R4),         dimension(:),   pointer :: stkve     => null()
     real(ESMF_KIND_R4),         dimension(:),   pointer :: factors   => null()
     type(ESMF_Field),           dimension(:),   pointer :: fields    => null()
-    type(aqm_internal_psrc_type), dimension(:), pointer :: ijmap     => null()
     type(aqm_internal_rate_type), dimension(:), pointer :: rates     => null()
     character(len=ESMF_MAXSTR), dimension(:,:), pointer :: table     => null()
   end type
@@ -73,7 +69,6 @@ module aqm_internal_mod
 
   public :: aqm_internal_data_type
   public :: aqm_internal_emis_type
-  public :: aqm_internal_psrc_type
   public :: aqm_internal_state_type
 
 end module aqm_internal_mod
